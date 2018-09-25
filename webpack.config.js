0d341e21d8939798f11d875740957534d20cbe88
Anantoliy Skakovskiy <toanatoliy@gmail.com>
2018-09-25 21:12:44 +0300

    added weatcher to webpack.config.js and plugin ProgressBarPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+
 
 module.exports = {
     entry: {
@@ -9,13 +12,29 @@ module.exports = {
         path: path.join(__dirname,'dist'),
         filename: 'js/[name].js'
     },
+    watch: true,
+    watchOptions: {
+        aggregateTimeout: 300,
+        poll: 1000,
+        ignored: path.join(__dirname, 'src/components/text.js')
+    },
     plugins: [
         new htmlWebpackPlugin({
             title: 'Vue-webpack template step by step',
-            filename: 'test.html',
+            filename: 'index.html',
             inject: true,
             hash: true,
-            template: 'index.html'
-        })
+            template: 'index.html',
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeAttributeQuotes: true
+
+            }
+        }),
+        new webpack.WatchIgnorePlugin([
+            path.join(__dirname,'node_modules')
+        ]),
+        new ProgressBarPlugin()
     ]
 }
